234fde22d4b2e8c3244f7a5df21e35059c7fb3a2
Kelly Silva <kellynha756@gmail.com>
2023-05-31 18:43:53 +0300

    make the ball be launch when user touch in the screen

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,9 +5,12 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody2D currentBallRigidBody;
+    [SerializeField] private float detachDelay = .5f;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private Rigidbody2D currentBallRigidBody;
+
     private Camera mainCamera;
+    private bool isDraggring;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,21 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (currentBallRigidBody == null) return;
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRigidBody.isKinematic = false;
+            if (isDraggring)
+            {
+                LaunchBall();
+            }
+
+            isDraggring = false;
+
             return;
         }
 
+        isDraggring = true;
         currentBallRigidBody.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
@@ -31,5 +43,19 @@ public class BallHandler : MonoBehaviour
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         currentBallRigidBody.position = worldPosition;
+    }
+
+    void LaunchBall()
+    {
+        currentBallRigidBody.isKinematic = false;
+        currentBallRigidBody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
