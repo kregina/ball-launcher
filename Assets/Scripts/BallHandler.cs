64b2ba9d998bd63b7a3f8f367abdde13019d9c0a
Kelly Silva <kellynha756@gmail.com>
2023-05-31 18:29:41 +0300

    toggle physics from currentBall

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,26 +5,31 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D currentBallRigidBody;
     private Camera mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main;    
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!Touchscreen.current.primaryTouch.press.isPressed)
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
+            currentBallRigidBody.isKinematic = false;
             return;
         }
+
+        currentBallRigidBody.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log("inhaaa" + worldPosition);
+        currentBallRigidBody.position = worldPosition;
     }
 }
